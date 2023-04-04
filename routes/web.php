c6b80897fdb1aa3f91c690490e683c073b0b6838
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-04-03 23:46:32 -0300

    Implement simple user visualization

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TicketController;
+use App\Models\User;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -33,6 +35,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/users', function () {
+    return Inertia::render('User/Index', ['users' => User::all()]);
 });
 
 require __DIR__.'/auth.php';
