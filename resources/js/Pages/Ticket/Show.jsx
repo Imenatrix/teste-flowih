5d053f8ebe540c4e68262f7ee0ae74d15e5ce1ba
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-04-04 04:55:16 -0300

    Add edit button on TIcket/Show

--- resources/js/Pages/Ticket/Show.jsx
+++ resources/js/Pages/Ticket/Show.jsx
@@ -1,5 +1,5 @@
 import { default as AuthenticatedLayout } from '@/Layouts/AuthenticatedLayout'
-import { Head, useForm } from '@inertiajs/react'
+import { Head, useForm, Link } from '@inertiajs/react'
 import TextInput from '@/Components/TextInput'
 import InputLabel from '@/Components/InputLabel'
 import InputError from '@/Components/InputError'
@@ -26,7 +26,16 @@ export default function (props) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">{ticket.title}</h2>}
+            header={
+                <>
+                    <h2 className="font-semibold text-xl text-gray-800 leading-tight">{ticket.title}</h2>
+                    {auth.user?.id == ticket.user_id &&
+                        <div className='flex justify-end'>
+                                <Link className='inline-flex items-center px-4 py-2 bg-yellow-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-400 focus:bg-orange-500 active:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150' href={route('tickets.create')}>Edit</Link>
+                        </div>
+                    }
+                </>
+            }
         >
             <Head title="Dashboard" />
 
